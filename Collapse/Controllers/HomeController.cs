6d4ced1dcdd9bd8036a1d5d1b2d85b8aaf6a396a
JooYoo <jooyoo@outlook.com>
2018-06-18 04:50:25 +0200

    created repo

---
+++ Collapse/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Collapse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = ListTest();
+            return View();
+        }
+
+
+        public List<string> ListTest()
+        {
+           return new List<string>{"string1","string2","string3","string4","string5"};
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
